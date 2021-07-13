7c8fb3557e19e6cfb1afd5f99c74123fa08cd1c3
David Castrillón <david.castri@hotmail.es>
2021-07-13 13:43:42 +0200

    13/07/2021 - Preparo estructura básica y programo la llamada a la API con ajax y con getJson. Saco dato por pantalla

---
+++ controllers/mainApp.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+
+    //Al cargarse la página hacemos ya la primera llamada AJAX y mostramos el primer chiste
+    ajaxCall();
+
+    //Cada vez que hagamos click en el botón, nos debe mostrar otro chiste
+    $("#ajaxBtn").click(function (e) {
+        e.preventDefault();
+
+        //Limpiamos el chiste anterior (no hace falta ya que sobrescribimos, crea un efecto de movimiento en la estructura al vaciarse el div)
+        // $("#joke").html("");
+
+        //Llamamos a la función con la llamada AJAX
+        ajaxCall();
+
+    });
+
+});
+
+/* Función que hace la llamada ajax */
+function ajaxCall() {
+
+    //Llamada con getJson
+    $.getJSON("http://api.icndb.com/jokes/random", function (data) {
+            $("#joke").html(data.value.joke);
+        }
+    );
+
+    //Llamada Ajax (otra opción, requiere más datos (incluye función callback en caso de error))
+    /*$.ajax({
+        type: "get", //Tipo get
+        url: "http://api.icndb.com/jokes/random", //URL de la API
+        dataType: "json", //Queremos que nos interprete los datos en json
+        success: function (data) { //En caso de éxito, nos sacará el chiste por pantalla
+            $("#joke").html(data.value.joke);
+        },
+        error: function (xhr, status, error) { //En caso de error, nos sacará por consola los datos necesarios para identificarlo
+            console.log(xhr);
+            console.log(status);
+            console.log(error);
+        }
+    });  */
+}
